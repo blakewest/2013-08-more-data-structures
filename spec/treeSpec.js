f91bbf523eb167cdbbe69ca1fb905fac792e5150
Hack Reactor Students <github@hackreactor.com>
2013-08-08 15:47:51 -0700

    set up functional shared tree, and made initial test pass

--- spec/treeSpec.js
+++ spec/treeSpec.js
@@ -10,6 +10,10 @@ describe("tree", function() {
     expect(tree.contains).toEqual(jasmine.any(Function));
     expect('value' in tree).toBe(true);
   });
+  
+  it("should return an empty array for tree.children on an empty tree", function() {
+    expect(tree.children).toEqual([]);
+  });
 
   // Add more tests here to test the functionality of tree.
 });
--- src/tree.js
+++ src/tree.js
@@ -1,14 +1,19 @@
-var makeTree = function(){
+var makeTree = function(value){
   var newTree = {};
-  newTree.value = undefined;
-  newTree.children = undefined;
-
+  newTree.value = value;
+  newTree.children = []; //will be an array
+  extend(newTree, treeMethods);
   return newTree;
+};
+var extend = function(to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
 };
 
 var treeMethods = {};
 
-treeMethods.addChild = function(){
+treeMethods.addChild = function(value){
 };
 
 treeMethods.contains = function(){
