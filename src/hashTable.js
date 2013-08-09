c29bcf28179069517260f30f3ebb27c1e3438ee5
Hack Reactor Students <github@hackreactor.com>
2013-08-08 18:25:44 -0700

    Gotta get back to lecture. Hash table maybe succeeding at collisions now

--- src/hashTable.js
+++ src/hashTable.js
@@ -1,5 +1,5 @@
 var HashTable = function(){
-  this._limit = 8;
+  this._limit = 1;
 
   // Use a limited array to store inserted elements.
   // It'll keep you from using too much space. Usage:
@@ -11,23 +11,50 @@ var HashTable = function(){
   // handy once you're working on resizing
   this._storage = makeLimitedArray(this._limit);
 };
-
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
   console.log(i);
-  this._storage.set(i, v);
+  if ( !this._storage.get(i) ) {
+    var newObj = {};
+    newObj[k] = v;
+    this._storage.set(i, newObj);
+  } else {
+    this._storage.get(i)[k] = v;
+  }
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.get(i);
+  if (this._storage.get(i)) {
+    return this._storage.get(i)[k];
+  } else {
+    return undefined;
+  }
 };
 
 HashTable.prototype.remove = function( key ){
   var i = getIndexBelowMaxForKey(key, this._limit);
   console.log(i);
-  this._storage.set(i, undefined);
+  delete this._storage.get(i)[key];
 };
+
+// HashTable.prototype.insert = function(k, v){
+//   var i = getIndexBelowMaxForKey(k, this._limit);
+//   console.log(i);
+//   this._storage.set(i, v);
+
+// };
+
+// HashTable.prototype.retrieve = function(k){
+//   var i = getIndexBelowMaxForKey(k, this._limit);
+//   return this._storage.get(i);
+// };
+
+// HashTable.prototype.remove = function( key ){
+//   var i = getIndexBelowMaxForKey(key, this._limit);
+//   console.log(i);
+//   this._storage.set(i, undefined);
+// };
 
 // NOTE: For this code to work, you will NEED the code from hashTableHelpers.js
 // Start by loading those files up and playing with the functions it provides.
