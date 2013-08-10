0d7fe7deb5efc4a1b635f5fd41cd3499cfbb25c6
Hack Reactor Students <github@hackreactor.com>
2013-08-10 10:53:18 -0700

    BST setup and passing a test

--- src/binarySearchTree.js
+++ src/binarySearchTree.js
@@ -1,2 +1,22 @@
-var makeBinarySearchTree = function(){
+var makeBinarySearchTree = function(value){
+	var newBST = Object.create(binaryMethods);
+  newBST.left = null;
+  newBST.right = null;
+  newBST.value = value || null;
+
+  return newBST;
+};
+
+var binaryMethods = {};
+
+binaryMethods.insert = function() {
+
+};
+
+binaryMethods.contains = function() {
+
+};
+
+binaryMethods.depthFirstLog = function() {
+
 };
