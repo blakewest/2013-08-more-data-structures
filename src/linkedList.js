80fb874b557efe7c3013066a6a164a2ff4aeb12b
Hack Reactor Students <github@hackreactor.com>
2013-08-10 16:46:06 -0700

    cleaned up linked list js file

--- src/linkedList.js
+++ src/linkedList.js
@@ -1,4 +1,3 @@
-// Note: don't use an array to do this.
 var makeLinkedList = function(){
   var list = {};
   list.head = null;
@@ -6,17 +5,13 @@ var makeLinkedList = function(){
   list.index = 0;
   list.start = 0;
 
-  //get rid of tracker (start), just use index (index) as the pointer for any item. 
-  //everytime you make a new node, index increases. 
-  //just use the 'head' and 'tail' as the pointers when you removeHead and removeTail
-
   list.removeHead = function(){
-    var result = list.head;
+    var oldHead = list.head;
 
-    if (!result) {
+    if (!oldHead) {
       return null;
-    }else if (result.next) {
-      var nextItem = list[result.next];
+    }else if (oldHead.next) {
+      var nextItem = list[oldHead.next];
 
       delete list[nextItem.previous];
 
@@ -29,39 +24,32 @@ var makeLinkedList = function(){
       delete list[list.findSoloNodeIndex()];
     }
 
-    return result.value;
+    return oldHead.value;
 
   };
 
   list.removeTail = function() {
-    // get tail
-    var result = list.tail;
-    
-    if (!result) {
+    var oldTail = list.tail;
+
+    if (!oldTail) {
       return null;
     }
     //only do if there is a previous item
-    else if (result.previous){   
-      var previousItem = list[result.previous];
+    else if (oldTail.previous){
+      var newTail = list[oldTail.previous];
 
       // delete the old tail
-      delete list[ previousItem.next ];
+      delete list[ newTail.next ];
 
-      // set the next value of the thing previous to tail to null
-      previousItem.next = null;
-      
-      // update tail property
-      list.tail = previousItem;
+      newTail.next = null;
+      list.tail = newTail;
 
     }else{  //this happens when there's only one item
       list.tail = null;
       list.head = null;
       delete list[list.findSoloNodeIndex()];
     }
-    
-    // return the old tail
-    return result.value;
-
+    return oldTail.value;
   };
 
   list.contains = function(value){
@@ -77,7 +65,7 @@ var makeLinkedList = function(){
     var newNode = makeNode(value);
     list[list.index] = newNode;
     list.tail = newNode;
-    if( !list.head ) {       // this sets head for very first item.
+    if( !list.head ) {       // this sets head if list is empty.
       list.head = newNode;
     }else {
       list[list.index-1].next = list.index;
@@ -88,28 +76,21 @@ var makeLinkedList = function(){
 
   list.addToHead = function(value) {
     var newNode = makeNode(value);
-
-    // get current head
     var currentHead = list.head;
-    // Shifting the head on a non-empty list
 
+    // Shifting the head on a non-empty list
     if ( currentHead !== null ) {
       // find neighbor of current head to determine currentHead's index, or find solo node index
       var currentHeadIndex = list.head.next ? list[list.head.next].previous : list.findSoloNodeIndex();
-      // set new node next to current head index
       newNode.next = currentHeadIndex;
-      // set current head previous to new nodes index
       currentHead.previous = list.index;
     } else {
       // set tail to newNode if list is empty
       list.tail = newNode;
     }
 
-    // place new node in list
     list[list.index] = newNode;
-    // reset head to new node
     list.head = newNode;
-    //increment index
     list.index++;
   };
 
